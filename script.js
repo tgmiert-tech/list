3c762f00f396ca5866098f23432d1ce198294ae5
tgmiert-tech <tgmiert@gmail.com>
2026-04-27 01:36:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const members = [
         role: "Владелец",
         description: "Владелец листа. вход бесплатно / галочка 15 звезд / закреп 25 звезд.",
         avatar: "img/avatar1.jpg",
+        hoverAvatar: "img/avatar1.gif", 
         verified: true,
         pinned: true,
         project: "https://t.me/+hgYBTlhzZOZmNDY0",
