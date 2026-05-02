a0c0b402493dda24234c884de4c7946677ee2a0a
tgmiert-tech <tgmiert@gmail.com>
2026-05-02 21:30:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const members = [
         role: "Владелец",
         description: "Владелец листа. вход бесплатно / галочка 15 звезд / закреп 25 звезд.",
         avatar: "img/avatar1.jpg",
-bg: "video/fon1.mp4", // bg: "video/fon1.mp4", // или false
-blur: 0, // процент не указывам 
+bg: "false", // bg: "video/fon1.mp4", // или false
+blur: 2, // процент не указывам 
         //если ты серьезно думаешь что это гпт написал ты чекнутый. это обычная заметка
-fon: "false", // или false либо fon: "fon/fon1.jpg",
+fon: "fon/fon1.jpg",, // или false либо fon: "fon/fon1.jpg",
         verified: true,
         pinned: true,
         color: "#206676",
