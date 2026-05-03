113c90936100364c48f5e4a5d418ca3a36ca3d5c
tgmiert-tech <tgmiert@gmail.com>
2026-05-03 12:32:46 +0300

    Add files via upload

---
+++ aricto.js
@@ -0,0 +1,14 @@
+// aricto.js - Второй лист "Aricto Session"
+const arictoMembers = [
+    {
+        id: 101,
+        nickname: "Пример",
+        username: "@example",
+        category: "Медийка",
+        role: "Участник",
+        description: "Описание человека",
+        avatar: "img/aricto/avatar101.jpg",
+        // ... все те же поля что и в members
+    },
+    // Здесь будут добавляться люди
+];
