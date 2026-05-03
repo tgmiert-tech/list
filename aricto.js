8694fae54cd292886ae33de3a47bc8c8e3f24a16
tgmiert-tech <tgmiert@gmail.com>
2026-05-03 14:34:16 +0300

    Update aricto.js

--- aricto.js
+++ aricto.js
@@ -27,3 +27,31 @@ fon: "fonAricto/fon1717.jpg", // или false либо fon: fonAricto/fon101.jpg
         }
     },  
 ];
+
+        {
+        id: 1718,
+        nickname: "тест",
+        username: "тест",
+        category: "Разработчики",
+        role: "Владелец",
+        description: "Меня зовут Аристослав. Я в Telegram с 2024 года; в КМ я не нахожусь и не находился. с 2018 по 2024 я был в дс в середине 24 попал в тг",
+        avatar: "imgAricto/avatar1717.jpg",
+bg: false, // bg: "videoAricto/fon101.mp4", // или false
+blur: 0, // процент не указывам 
+        //если ты серьезно думаешь что это гпт написал ты чекнутый. это обычная заметка
+fon: "fonAricto/fon1717.jpg", // или false либо fon: fonAricto/fon101.jpg,
+        verified: true,
+        pinned: true,
+        color: "#A64300",
+        project: "https://t.me/ArictoSession",
+        telegram: "faymovy",
+        joinDate: "2026-05-05",
+        activity: "Постоянная",
+        details: " Я  разработчик бесплатного юзер бота ArictoSession по ссылке ArictoBypass.fun (http://arictobypass.fun/). Ранее был разработчиком проектов по типу ArictoSearch (бесплатный шерлок) ArictoFame и ArictoStat (аналог Telelog) Сейчас продвигаю проект под названием ArictoSession.",
+        skills: ["Администрация", "кодер"],
+        socials: {
+            telegram: "@faymovy",
+            project: "https://t.me/ArictoSession",
+        }
+    },  
+];
