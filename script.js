99a265e3ce19951a438ffec47c44115a2c4af5c9
tgmiert-tech <tgmiert@gmail.com>
2026-05-03 04:38:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,33 @@ fon: "fon/fon2.jpg", // или false либо fon: "fon/fon1.jpg",
             telegram: "@swatolk",
             project: "https://t.me/+lCld3oGs4ewxOTRh",
         }
-    },   
+    }, 
+        {
+        id: 3,
+        nickname: "Ар¡сто?",
+        username: "@faymovy",
+        category: "Модерация",
+        role: "Владелец",
+        description: "Меня зовут Аристослав. Я в Telegram с 2024 года; в КМ я не нахожусь и не находился. с 2018 по 2024 я был в дс в середине 24 попал в тг",
+        avatar: "img/avatar3.jpg",
+bg: false, // bg: "video/fon1.mp4", // или false
+blur: 0, // процент не указывам 
+        //если ты серьезно думаешь что это гпт написал ты чекнутый. это обычная заметка
+fon: "fon/fon3.jpg", // или false либо fon: "fon/fon1.jpg",
+        verified: true,
+        pinned: true,
+        color: "#206676",
+        project: "https://t.me/ArictoSession",
+        telegram: "faymovy",
+        joinDate: "2026-05-05",
+        activity: "Постоянная",
+        details: " Я  разработчик бесплатного юзер бота ArictoSession по ссылке ArictoBypass.fun (http://arictobypass.fun/). Ранее был разработчиком проектов по типу ArictoSearch (бесплатный шерлок) ArictoFame и ArictoStat (аналог Telelog) Сейчас продвигаю проект под названием ArictoSession.",
+        skills: ["Администрирование", "кодер"],
+        socials: {
+            telegram: "@faymovy",
+            project: "https://t.me/ArictoSession",
+        }
+    },  
 ];
 
 let currentTheme = 'black';
